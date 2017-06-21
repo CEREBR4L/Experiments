673fcd3068e8f5188e5e17753c42af5065c32ddc
CEREBR4L <mattmillergraphics@gmail.com>
2017-06-21 18:42:54 +0100

    rand num gen..........

--- react/Basics/game/src/Quiz.js
+++ react/Basics/game/src/Quiz.js
@@ -6,16 +6,30 @@ class Quiz extends Component{
     constructor(){
         super()
 
-        let riddle = {
-            resultsArray: [8, 9, 10, 11],
-            field1: 5,
-            field2: 5,
-            answer: 10
-        }
+        let riddle = this.playGame()
 
         this.state= { riddle }
 
         this.renderOptions = this.renderOptions.bind(this)
+        this.playGame = this.randNo.bind(this)
+    }
+
+    randNo(min, max){
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    playGame(){
+        let field1 = this.randNo(20, 50)
+        let field2 = this.randNo(20, 50)
+        let answer = field1 + field2
+        let riddle = {
+            resultsArray: [8, 9, 10, 11],
+            field1: field1,
+            field2: field2,
+            answer: answer
+        }
+
+        return riddle
     }
 
     renderOptions(){
@@ -32,7 +46,7 @@ class Quiz extends Component{
         return(
             <div className="quiz">
                 <div className="quiz-content">
-                    <p className="question">What is the sum of <span className="text-info">5</span> and <span className="text-info">5</span>?</p>
+                    <p className="question">What is the sum of <span className="text-info">{this.state.riddle.field1}</span> and <span className="text-info">{this.state.riddle.field2}</span>?</p>
                     {this.renderOptions()}
                 </div>
                 <div className="play-again">
