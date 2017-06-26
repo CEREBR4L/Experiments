20054a03001c2202d47adce6bfbdf684303a15d2
CEREBR4L <mattmillergraphics@gmail.com>
2017-06-26 18:39:32 +0100

    Start of game two...

---
+++ react/Basics/stupid-game-2/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import EasyABC from './EasyABC'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>GAME LOL</h2>
+        </div>
+        <EasyABC />
+      </div>
+    );
+  }
+}
+
+export default App;
