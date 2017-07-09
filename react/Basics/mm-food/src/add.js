ac9641917b24c5e02088d77c88dbd78265082ea1
CEREBR4L <mattmillergraphics@gmail.com>
2017-07-09 15:29:20 +0100

    Adding submit form

--- react/Basics/mm-food/src/add.js
+++ react/Basics/mm-food/src/add.js
@@ -16,10 +16,36 @@ class Add extends Component {
     }
   render() {
     return (
-      <div className="container">
-        <h1>Add</h1>
-        <button onClick={this.goHome}>Submit</button>
-      </div>
+        <div classNameName="row">
+            <div classNameName="col-xs-12">
+                <h1>Add Recipe</h1> 
+                <form>
+                    <div className="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" className="form-control" id="name" placeholder="Recipe Name" />
+                    </div>
+                    <div className="form-group">
+                        <label for="desc">Description</label>
+                        <textarea className="form-control" id="desc" placeholder="Enter description" />
+                    </div>
+                    <div className="form-group col-xs-5">
+                        <label for="qty">Quantity</label>
+                        <input type="text" className="form-control" id="qty" placeholder="Enter description" />
+                    </div>
+                    <div className="form-group col-xs-5">
+                        <label for="item">Ingredient</label>
+                        <input type="text" className="form-control" id="item" placeholder="Enter description" />
+                    </div>
+                    <div className="col-xs-2">
+                        <label for="item">Add</label>
+                        <button type="submit" className="btn btn-info form-control">Add</button>
+                    </div>
+                    <div className="col-xs-12">
+                        <button type="submit" className="btn btn-default">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
   }
 }
